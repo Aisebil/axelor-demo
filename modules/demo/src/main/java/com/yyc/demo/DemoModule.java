83492944cf81e78164c6efd8a04d79efd16e5862
陈逸洋41151 <yiyang.chen01@hand-china.com>
2024-03-08 17:47:22 +0800

    20240308

--- modules/demo/src/main/java/com/yyc/demo/DemoModule.java
+++ modules/demo/src/main/java/com/yyc/demo/DemoModule.java
@@ -2,13 +2,10 @@ package com.yyc.demo;
 
 import com.axelor.app.AxelorModule;
 
-import com.yyc.demo.service.*;
-import com.yyc.demo.service.impl.*;
+//import com.yyc.demo.service.*;
+//import com.yyc.demo.service.impl.*;
 
 public class DemoModule extends AxelorModule {
 
-    @Override
-    protected  void configure() {
-        bind(HumanService.class).to(HumanServiceImpl.class);
-    }
+
 }
